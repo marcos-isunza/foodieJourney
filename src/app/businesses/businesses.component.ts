d0d8ddcf212668b3130b37a42de427d72668e99e
Marcos Isunza <marcos.isunza.ibarra@accenture.com>
2018-02-12 09:44:39 -0600

    added loading spinner and local storage

--- src/app/businesses/businesses.component.ts
+++ src/app/businesses/businesses.component.ts
@@ -1,12 +1,8 @@
 import { Component, OnInit, OnChanges } from '@angular/core';
-import {
-  YelpServiceComponent
-} from '../service/yelp-service';
-import {
-  Business
-} from '../business-interface';
-import {ActivatedRoute, Router} from '@angular/router';
-import {NgbTabsetConfig} from '@ng-bootstrap/ng-bootstrap';
+import { YelpServiceComponent } from '../service/yelp-service';
+import { Business } from '../business-interface';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgbTabsetConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-businesses',
@@ -18,74 +14,84 @@ export class BusinessesComponent implements OnInit {
   TitleJournal;
   DescJournal;
   text_tittle: boolean;
-  constructor(private _businessService: YelpServiceComponent, private _route: ActivatedRoute, private _router: Router, config: NgbTabsetConfig) {
-     console.log(this._route.snapshot.paramMap.get('ctg'));
-      config.justify = 'start';
-      config.type = 'tabs';
+  constructor(
+    private _businessService: YelpServiceComponent,
+    private _route: ActivatedRoute,
+    private _router: Router,
+    config: NgbTabsetConfig
+  ) {
+    config.justify = 'start';
+    config.type = 'tabs';
   }
 
-var_show: boolean;
-var_show1: boolean;
+  var_show: boolean;
+  var_show1: boolean;
   business: Business[] = [];
   errorMessage: string;
   geolocationPosition = {};
   postNumber = 0;
-  generatePostNumber(){
+  defaultImage = '../../assets/no-image.jpg';
 
-    this.postNumber = this.postNumber +1;
+  category = this._route.snapshot.paramMap.get('ctg');
+
+  storeLocation(location) {
+    localStorage.setItem('latitude', JSON.stringify(location['latitude']));
+    localStorage.setItem('longitude', JSON.stringify(location['longitude']));
   }
 
- category = this._route.snapshot.paramMap.get('ctg');
   ngOnInit() {
     //get Geolocation
-        if (window.navigator && window.navigator.geolocation) {
-        window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.geolocationPosition = position.coords,
-                    this._businessService.getBusinessByLocation(this.geolocationPosition, this.category)
-                    .subscribe(business => {
-                      this.business = business;
-                      //this.generatePostNumber();
-                    },
-                    error => this.errorMessage = < any > error);
-                    ;
-            },
-            error => this.errorMessage = < any > error);
+    if (window.navigator && window.navigator.geolocation) {
+      window.navigator.geolocation.getCurrentPosition(position => {
+        (this.geolocationPosition = position.coords),
+          this.storeLocation(this.geolocationPosition),
+          this._businessService
+            // .getBusinessByLocation(this.geolocationPosition, this.category)
+            .getBusinessByLocation(this.category)
+            .subscribe(business => {
+              this.business = business;
+            }, error => (this.errorMessage = <any>error));
+      }, error => (this.errorMessage = <any>error));
     }
   }
-  public f_click(id: number){
-    switch(id) { 
-      case 1: { 
+  public f_click(id: number) {
+    switch (id) {
+      case 1: {
         this.text_tittle = true;
-        this.TitleJournal = "American";
-          this.DescJournal = "One characteristics of America cooking is the fusion of multiple ethnic or regional approaches into completely new cooking styles" 
+        this.TitleJournal = 'American';
+        this.DescJournal =
+          'One characteristics of America cooking is the fusion of multiple ethnic or regional approaches into completely new cooking styles';
         console.log(this.text_tittle);
-         break; 
-      } 
-      case 2: { 
-        this.text_tittle = true;
-        this.TitleJournal = "Korean";
-        this.DescJournal = "One characteristics of America cooking is the fusion of multiple ethnic or regional approaches into completely new cooking styles" 
-         break; 
+        break;
       }
-      case 3: { 
+      case 2: {
         this.text_tittle = true;
-        this.TitleJournal = "Italian";
-        this.DescJournal = "One characteristics of America cooking is the fusion of multiple ethnic or regional approaches into completely new cooking styles" 
-         break; 
-      } 
-      case 4: { 
-        this.text_tittle = true;
-        this.TitleJournal = "Mexican";
-        this.DescJournal = "One characteristics of America cooking is the fusion of multiple ethnic or regional approaches into completely new cooking styles" 
-         break; 
+        this.TitleJournal = 'Korean';
+        this.DescJournal =
+          'One characteristics of America cooking is the fusion of multiple ethnic or regional approaches into completely new cooking styles';
+        break;
       }
-      case 5: { 
+      case 3: {
         this.text_tittle = true;
-        this.TitleJournal = "Japanese";
-        this.DescJournal = "One characteristics of America cooking is the fusion of multiple ethnic or regional approaches into completely new cooking styles" 
-         break; 
-      }   
-   } 
+        this.TitleJournal = 'Italian';
+        this.DescJournal =
+          'One characteristics of America cooking is the fusion of multiple ethnic or regional approaches into completely new cooking styles';
+        break;
+      }
+      case 4: {
+        this.text_tittle = true;
+        this.TitleJournal = 'Mexican';
+        this.DescJournal =
+          'One characteristics of America cooking is the fusion of multiple ethnic or regional approaches into completely new cooking styles';
+        break;
+      }
+      case 5: {
+        this.text_tittle = true;
+        this.TitleJournal = 'Japanese';
+        this.DescJournal =
+          'One characteristics of America cooking is the fusion of multiple ethnic or regional approaches into completely new cooking styles';
+        break;
+      }
+    }
   }
 }
--- src/app/challenges/challenges.component.ts
+++ src/app/challenges/challenges.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./challenges.component.css']
 })
 export class ChallengesComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  challengeProgress = 0;
+  challengeWidth = 0;
 
-  ngOnInit() {
+  checkProgress() {
+    if (localStorage.getItem('challenge1') != null) {
+      this.challengeProgress = 20;
+      this.challengeWidth = 20 * 196 / 100;
+    }
   }
 
+  ngOnInit() {
+    this.checkProgress();
+  }
 }
